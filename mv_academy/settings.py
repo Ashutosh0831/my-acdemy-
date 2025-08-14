09735ac980caf049c79f852cdf878517bc383230
Ashutosh0831 <bhardwaj.83241@gmail.com>
2025-08-15 00:22:30 +0530

    Fix STATIC_ROOT and enable WhiteNoise

--- mv_academy/settings.py
+++ mv_academy/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,6 +140,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+STATICFILES_STROAGE = 'whitenoise.storage.CompressedManifestStaticFilesStroage'
 
 
 # Media files (User-uploaded content)
