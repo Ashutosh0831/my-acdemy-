9e4f1ab41f8866031657b8fd564809a97c2a9a56
Ashutosh0831 <bhardwaj.83241@gmail.com>
2025-09-27 22:00:34 +0530

    js changes

--- static/JavaScript/header.js
+++ static/JavaScript/header.js
@@ -1,27 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let lastScrollTop = 0;
-  const header = document.querySelector(".header-container");
-
-  function handleScroll() {
-    const isMobile = window.innerWidth <= 768;
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (isMobile && header) {
-      if (scrollTop - lastScrollTop > 10) {
-        header.classList.add("hide-on-scroll");
-      } else if (lastScrollTop - scrollTop > 2) {
-        header.classList.remove("hide-on-scroll");
-      }
-      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-    } else if (header) {
-      header.classList.remove("hide-on-scroll");
-    }
-  }
-
-  window.addEventListener("scroll", handleScroll);
-});
-
-document.addEventListener("DOMContentLoaded", function () {
   const moreBtn = document.getElementById("moreBtn");
   const navList = document.getElementById("navList");
 
@@ -30,5 +7,3 @@ document.addEventListener("DOMContentLoaded", function () {
     moreBtn.classList.toggle("open");
   });
 });
-
-
